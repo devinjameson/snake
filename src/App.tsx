0759b60925e9f6a388008284ee516934ff951d65
Devin Jameson <devinjameson@hey.com>
2023-09-24 16:21:39 -0400

    Add game board

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,37 @@
-const App = (): JSX.Element => {
-  return <h1>App</h1>;
-};
+import * as E from 'effect'
 
-export default App;
+type Board = E.Chunk.Chunk<Row>
+
+type Row = E.Chunk.Chunk<Cell>
+
+type Cell = [number, number]
+
+const BOARD_SIZE = 20
+
+const board: Board = E.Chunk.makeBy(BOARD_SIZE, (x) =>
+  E.Chunk.makeBy(BOARD_SIZE, (y) => [x, y]),
+)
+
+const App = (): JSX.Element => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="border-black border divide-y divide-black">
+        {board.pipe(
+          E.Chunk.map((row, rowIdx) => {
+            return (
+              <div key={rowIdx} className="flex divide-x divide-black">
+                {row.pipe(
+                  E.Chunk.map((_, colIdx) => {
+                    return <div key={colIdx} className="w-10 h-10"></div>
+                  }),
+                )}
+              </div>
+            )
+          }),
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default App
